369731a873e6f9d1b81c3bbfe32224898694a3a0
taqwa96 <t.jadedu@edu.letteverein.berlin>
2023-10-10 15:19:58 +0200

    Alle Methoden in Schmuck implementiert. Und ein Test für getBezeichnung gemacht(Von Herrn Roussel)

--- Test/stuff/SchmuckTest.java
+++ Test/stuff/SchmuckTest.java
@@ -1,6 +1,9 @@
 package stuff;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
@@ -8,5 +11,15 @@ public class SchmuckTest {
 
     @Test
     public void getBezeichnung() {
+
+        Collection<Schmuck> schatz = Schatztruhe.getSchatz();
+
+        for (Schmuck schmuck : schatz) {
+            Assert.assertNotNull("Die Bezeichnung von " + schmuck.getClass().getName() + " ist null", schmuck.getBezeichnung());
+        }
+    }
+
+    @Test
+    public void getMaterial() {
     }
 }
