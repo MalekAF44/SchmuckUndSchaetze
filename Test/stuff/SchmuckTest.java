cac94709d21d9f1acf7aab3f3e6c2b2dad1ff7b8
m.fattah <m.fattah@edu.letteverein.berlin>
2023-10-17 11:53:10 +0200

    Schmucktest - getKatalognummerTEST Checkt ob es gleiche Katalognummer gibt und ob die nummer negativ is

--- Test/stuff/SchmuckTest.java
+++ Test/stuff/SchmuckTest.java
@@ -22,4 +22,34 @@ public class SchmuckTest {
     @Test
     public void getMaterial() {
     }
+
+
+    @Test
+    public void getKatalognummer() {
+        Collection<Schmuck> schatz = Schatztruhe.getSchatz();
+
+        for (Schmuck schmuck : schatz) {
+            int katalognummer = schmuck.getKatalognummer();
+            Assert.assertTrue("Die Katalognummer von " + schmuck.getClass().getName() + " ist negativ: " + katalognummer, katalognummer >= 0);
+        }
+
+        Assert.assertTrue("Die Katalognummern sind nicht eindeutig.", KatalogNummerVergleich(schatz));
+    }
+    private boolean KatalogNummerVergleich(Collection<Schmuck> schatz) {
+        for (Schmuck schmuck1 : schatz) {
+            int katalognummer1 = schmuck1.getKatalognummer();
+
+            for (Schmuck schmuck2 : schatz) {
+                if (schmuck1 != schmuck2) {
+                    int katalognummer2 = schmuck2.getKatalognummer();
+
+                    if (katalognummer1 == katalognummer2) {
+                        return false; // Es gibt eine nicht-eindeutige Katalognummer.
+                    }
+                }
+            }
+        }
+
+        return true; // Alle Katalognummern sind eindeutig.
+    }
 }
