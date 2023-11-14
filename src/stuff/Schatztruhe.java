e49ef11af6874925663fdec18034fed34a31165c
c.roussel <kleppi@gmx.de>
2023-11-14 14:03:45 +0100

    Erweiterung der Schatztruhe zum filtern der Schmuckstücke

--- src/stuff/Schatztruhe.java
+++ src/stuff/Schatztruhe.java
@@ -5,6 +5,7 @@ import org.reflections.Reflections;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Die stuff.Schatztruhe beinhaltet eine Schatz, der aus jeweils einem Objekt von jeder Klasse besteht,
@@ -12,11 +13,34 @@ import java.util.Set;
  */
 public class Schatztruhe {
 
+    enum Schmuckfilter{
+        ALLES,
+        NUR_FALSCHE,
+        NUR_RICHTIGE
+    }
 
-
+    //TODO: Diesen Wert ändern, wenn Ihr die Tests nur für bestimmte Schmuckstücke laufen lassen wollt.
+    private static final Schmuckfilter filter = Schmuckfilter.ALLES;
 
 
     public static Collection<Schmuck> getSchatz(){
+
+        switch (filter){
+
+
+            case NUR_FALSCHE:
+                return getSchatzInternal().stream().filter(schmuck -> schmuck.getClass().getName().endsWith("F")).collect(Collectors.toList());
+            case NUR_RICHTIGE:
+                return getSchatzInternal().stream().filter(schmuck -> !schmuck.getClass().getName().endsWith("F")).collect(Collectors.toList());
+            case ALLES:
+            default:
+                return getSchatzInternal();
+        }
+    }
+
+
+
+    private static Collection<Schmuck> getSchatzInternal(){
 
 
         Reflections reflections = new Reflections("schmuckstuecke");
